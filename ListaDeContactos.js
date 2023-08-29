d086602bfb7ec534e02ff7891c59174d2800ff90
EdgardoPerdomo <ed1997cast@gmail.com>
2023-08-28 22:00:54 -0600

    Lista de Contactos en JavaScript 2

--- ListaDeContactos.js
+++ ListaDeContactos.js
@@ -1,47 +1,63 @@
 //1.Crea una lista de contactos con datos predefinidos, cada contacto debe contener el nombre y apellido
 let listContactos = [
     {id: 1,
-    nombre: "Edgardo",
-    apellido: "Perdomo",
-    numero: 50495392589
+    nombres: "Ezer Edgardo",
+    apellidos: "Perdomo Castellanos",
+    telefono: 50495392589,
+    ubicaciones: [{
+        departamento: "Santa Barbara",
+        municipio: "Santa Barbara"
+    }]
     },
     {id: 2,
-    nombre: "Kensy",
-    apellido:"Muñoz",
-    numero: 50496879689
+    nombres: "Kensy",
+    apellidos: "Muñoz Dubon",
+    telefono: 50495392649,
+    ubicaciones: [{
+        departamento: "Santa Barbara",
+        municipio: "Ilama"
+    }]
     },
     {id: 3,
-    nombre: "Carlos",
-    apellido:"Hernandez",
-    numero: 50494530899
+    nombres: "Carlos Edgardo",
+    apellidos: "Hernandez",
+    telefono: 50405382589,
+    ubicaciones: [{
+        departamento: "Copan",
+        municipio: "Santa Rosa Copan"
+    }]
     },
     
 ]
 
 //2.Crea una función para añadir un nuevo contacto
-function añadir(idN, nombreN, apellidoN, numeroN){
+function añadir(idN, nombresN, apellidosN, telefonoN, departamentoN, municipioN){
     const contactoNuevo = {
         id: idN,
-        nombre: nombreN,
-        apellido: apellidoN,
-        telefono: numeroN,
+        nombres: nombresN,
+        apellido: apellidosN,
+        telefono: telefonoN,
+        ubicaciones: [{
+            departamento: departamentoN,
+            municipio: municipioN
+        }]
     }
     listContactos.push(contactoNuevo)
     return (listContactos)
 
 }
-añadir( 4, "Adhara", "Muñoz", 50495392589)
+añadir( 4, "Adhara", "Muñoz", 50495392589,"Ocotepeque", "Sinuapa")
 
 //3.Crea una función para borrar un contacto existente
-function eliminar(nombreE){
+function eliminar(idE){
     const listaNueva = listContactos.filter((contacto)=>{
-        if(contacto.nombre !== nombreE){
+        if(contacto.id !== idE){
             return contacto;
         }
     } )  
     listContactos=listaNueva  
 }
-eliminar("Carlos");
+eliminar(3);
 
 //4.Crea una función para imprimir en consola los contactos presentes en la lista
 function mostrar(){
